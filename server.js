bfc60ca264ef90d37eb9d3f3435ffe031d6c16c1
bavertu <141646959+bavertu@users.noreply.github.com>
2024-09-01 16:01:49 +0300

    app commit

---
+++ server.js
@@ -0,0 +1,34 @@
+const path = require('path');
+const express = require('express');
+const cors = require('cors');
+require('dotenv').config();
+const port = process.env.PORT || 5001;
+const connectDB = require('./config/db');
+
+connectDB();
+
+const app = express();
+
+// Static Folder
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Body parser middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+// cors middleware
+app.use(
+  cors({
+    origin: ['http://localhost:5001', 'http://localhost:3001'],
+    credentials: true,
+  })
+);
+
+app.get('/', (req, res) => {
+  res.json({ message: 'Welcome to the RandomIdeas API' });
+});
+
+const ideasRouter = require('./routes/ideas');
+app.use('/api/ideas', ideasRouter);
+
+app.listen(port, () => console.log(`Server listening on port ${port}`));
